9b9a4ea0d6a974d7112efda57c6c2f3fd60bd627
Dani-Darko <2115589@swansea.ac.uk>
2023-05-11 02:37:33 +0100

    Ignore Files

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QApplication
 import main_window
 import sys
 
 class MainWindow(QMainWindow, main_window.Ui_MainWindow):
-    def __init__(self, parent=None)
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-	self.setupUi(self)
+        self.setupUi(self)
 
         self.doSomething.clicked.connect(self.something)
-        self.slidyboi.valueChanged.connect(self.update_spinny_boi)
+        self.SlidyBoi.valueChanged.connect(self.update_spinny_boi)
 
     def something(self):
-	self.status
+        self.lol.setText("SOMETHING IS HAPPENING. Oh. It didn't.")
     def update_spinny_boi(self, value):
-
+        self.spinyBoi.setValue(value)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
