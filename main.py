8ad12762f4264f9a97a207fb227b224bea4d5c20
Dani-Darko <2115589@swansea.ac.uk>
2023-06-07 23:53:22 +0100

    New UI window geometry size
    The buttons disappeared, the window is resized to standard size 600x800 px, first sketch of main window to start in next iteration of UI development.

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ class MainWindow(QMainWindow, main_window.Ui_MainWindow):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
 
-        self.doSomething.clicked.connect(self.something)
-        self.SlidyBoi.valueChanged.connect(self.update_spinny_boi)
-
-    def something(self):
-        self.lol.setText("SOMETHING IS HAPPENING. Oh. It didn't.")
-    def update_spinny_boi(self, value):
-        self.spinyBoi.setValue(value)
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main = MainWindow()
